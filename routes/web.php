0440aed39e7484f2b3fbf483c60b26a4aab30468
okumura <ef@ef-tech.jp>
2020-09-25 01:23:30 +0900

    ログイン中に　"/" へアクセスした場合に "/dashboard" にリダイレクト

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // ログイン中に　"/" へアクセスした場合に "/dashboard" にリダイレクトする
+    if (Auth::check()) {
+        return redirect()->route('dashboard');
+    }
+
     return view('welcome');
 });
 
